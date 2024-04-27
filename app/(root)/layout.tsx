a4a37d82a3e1093ee620ad49cd21cfe33c8c264d
Vansh Comar <vanshcomar16@gmail.com>
2024-04-27 20:44:06 +0530

    Create general layout as part of the state design process

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Topbar from "../../components/shared/Topbar";
+import LeftSidebar from "../../components/shared/LeftSidebar";
+import RightSidebar from "../../components/shared/RightSidebar";
+import Bottombar from "../../components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,15 +26,13 @@ export default function RootLayout({
           <Topbar />
           <main>
             <LeftSidebar />
-            <section className={'main-containter'}>
-                <div className="w-full max-w-4xl">
-                  
-                </div>
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
             </section>
             <RightSidebar />
           </main>
           <Bottombar />
-          </body>
+        </body>
       </html>
     </ClerkProvider>
   );
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,3 @@
+export default function RideSidebar() {
+  return <h1>RideSidebar</h1>;
+}
