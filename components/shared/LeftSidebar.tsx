38d8247d3f4d5e42a4d5fbe60f50a596866326f6
vanshcomar <vanshcomar1@gmail.com>
2024-06-10 21:00:04 +0530

    Part LeftSideBar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,3 +1,9 @@
 export default function LeftSidebar() {
-  return <h1>LeftSidebar</h1>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks}
+      </div>
+    </section>
+  );
 }
